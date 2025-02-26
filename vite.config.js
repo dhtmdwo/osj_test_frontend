3990b87a07309c61f2be4b3b569dac43ae833d7e
seungjae <dhtmdwo73@naver.com>
2025-02-26 15:11:25 +0900

    [fix] IP주소 변경

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://192.0.3.46:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
